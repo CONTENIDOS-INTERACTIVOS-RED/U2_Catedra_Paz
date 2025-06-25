dc4f38f3e8e9a757be62bda6202ef060c4d2f810
Francisco Lizcano <flizcanor@icloud.com>
2025-06-25 00:02:19 -0500

    ajustes finales

--- src/views/curso/Tema4.vue
+++ src/views/curso/Tema4.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   .curso-main-container.pb-3
-    BannerInterno(:titulo="'Unidad 2. Teorías y enfoques para la resolución pacífica de conflictos'" :subTitulo="'SINTESIS'")
+    BannerInterno(:titulo="'Unidad 2. Teorías y enfoques para la resolución pacífica de conflictos'" :subTitulo="'SÍNTESIS'")
     .container.tarjeta.tarjeta--blanca.p-4.p-md-5
       
       p(data-aos="fade-up").mb-5 La Unidad 2 "Teorías y enfoques para la resolución pacífica de conflictos" proporciona los fundamentos teóricos y prácticos necesarios para comprender y gestionar los conflictos de manera constructiva. A través del estudio de teorías clásicas y contemporáneas, como las propuestas por Karl Marx, Max Weber y Lewis Coser, se analiza el origen y las dinámicas de los conflictos sociales, económicos y políticos. Además, se presentan enfoques prácticos como la negociación, mediación y arbitraje, que ayudan a transformar los desacuerdos en oportunidades de cooperación y entendimiento. Esta unidad es clave para desarrollar habilidades que fomenten la convivencia pacífica y la colaboración en diversos entornos.
